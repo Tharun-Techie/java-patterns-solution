09a9093899e220902a15e1f3e5815e491582e9e4
Tharun R <Tharuntechie@gmail.com>
2024-09-15 20:44:11 +0530

    Rectangle added

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,5 +1,15 @@
 public class Rectangle {
     public static void main(String[] args){
-        System.out.println("Hello Boys");
+//        ****
+//        ****
+//        ****
+//        ****
+        int n = 4;
+        for(int row = 1;row<=n;row++){
+            for (int col = 1; col <=n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
