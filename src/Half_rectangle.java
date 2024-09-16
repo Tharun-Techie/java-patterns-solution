a755acbb20897054616d321227aea75ef63e6614
Tharun R <Tharuntechie@gmail.com>
2024-09-16 13:02:02 +0530

    Solutions added

--- src/Half_rectangle.java
+++ src/Half_rectangle.java
@@ -1,7 +1,19 @@
+import java.sql.SQLOutput;
+
 public class Half_rectangle {
 //    *
-////    **
-////    ***
-////    ****
-////    *****
+//    **
+//    ***
+//    ****
+//    *****
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int row = 1; row <=n; row++) {
+            for (int col =1;col<=row;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
