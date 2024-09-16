cd9fd8389aff37c4f3443c1c5ad109c9d0de3c8f
Tharun R <Tharuntechie@gmail.com>
2024-09-16 13:54:21 +0530

    some problem is there

--- src/Left_Half_rectangle.java
+++ src/Left_Half_rectangle.java
@@ -1,2 +1,17 @@
 public class Left_Half_rectangle {
+//    *****
+//    ****
+//    ***
+//    **
+//    *
+
+    public static void main(String[] args) {
+        int n = 5;
+        for(int row = 1;row<=n; row--){
+            for(int col = n;col<=n-row; col--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
