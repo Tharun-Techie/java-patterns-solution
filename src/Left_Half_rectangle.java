977533e4cb09ddd933b89df0aba218c0a9c7ed56
Tharun R <Tharuntechie@gmail.com>
2024-09-17 21:17:23 +0530

    finally solved thanks kunal

--- src/Left_Half_rectangle.java
+++ src/Left_Half_rectangle.java
@@ -7,8 +7,8 @@ public class Left_Half_rectangle {
 
     public static void main(String[] args) {
         int n = 5;
-        for(int row = 1;row<=n; row--){
-            for(int col = n;col>=1; col--){
+        for(int row = 1;row<=n; row++){
+            for(int col = 1; col<=n-row+1;col++){
                 System.out.print("*");
             }
             System.out.println();
